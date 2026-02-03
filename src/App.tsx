6e16920ee6784a615e41d6339d03a9e9289d1b1c
이승호 <seungho5623@gmail.com>
2026-02-03 21:29:13 +0900

    BrowserRouter에서 HashRouter로 변경
    BrowserRouter에서 HashRouter로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AppRoutes } from './routes/AppRoutes';
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       {/* GlobalStyles is rendered in main.tsx, but can also be here if we want it inside Router context or similar. 
           Currently main.tsx handles it. */}
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
