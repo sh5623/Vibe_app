f5a3aee445bd02c353d6a3e6a01e1b13e55129ee
이승호 <seungho5623@gmail.com>
2026-03-02 13:32:30 +0900

    sylted 컴포넌트 분리
    sylted 컴포넌트 분리

---
+++ src/app/styled.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  gap: 20px;
+`;
